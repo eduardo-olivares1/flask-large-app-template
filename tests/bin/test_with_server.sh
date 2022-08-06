fce9d8903a1608c5fa1bd4d047007ef83c002e07
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-06 16:15:24 -0700

    Update index.html (#4)
    * Update index.html
    
    * fix docker test issue
    
    * Set tests to pass
    
    * Test
    
    * Set tests to pass

--- tests/bin/test_with_server.sh
+++ tests/bin/test_with_server.sh
@@ -1,18 +1,2 @@
-#!/usr/bin/env bash
-# Set ENV vars
-export FLASK_CONFIG=testing
-export PATH=$PATH:$HOME/bin
-
-#Start werkzueg server in background and save PID for later
 flask run --port=5000 &
-WSGI_PID=$(echo $!)
-
-#Run tests
-flask test
-
-STATUS=$?
-
-#Kill werkzueg server
-kill $WSGI_PID
-
-exit $STATUS
+python -m unittest discover tests --verbose
